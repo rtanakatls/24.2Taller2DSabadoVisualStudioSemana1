f4e8658eea47b47ff1bf9dd27c458f46d0e6be39
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-08-24 09:46:24 -0500

    First commit

---
+++ Menu.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Taller2DSabadoSemana1
+{
+    internal class Menu
+    {
+        public void Execute()
+        {
+            EnterName();
+            EnterAge();
+        }
+
+        private void EnterName()
+        {
+            string name;
+            Console.WriteLine("Introduce tu nombre");
+            name = Console.ReadLine();
+            Console.WriteLine($"Hola {name}");
+        }
+
+        private void EnterAge()
+        {
+            int age;
+            bool continueFlag = true;
+
+            while (continueFlag)
+            {
+                Console.WriteLine("Introduce tu edad");
+                age = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tienes {age} años");
+                if (age < 0)
+                {
+                    Console.WriteLine("No puedes tener edad negativa");
+                }
+                else if (age < 18)
+                {
+                    continueFlag = false;
+                    Console.WriteLine("Eres menor de edad");
+                }
+                else
+                {
+                    continueFlag = false;
+                    Console.WriteLine("Eres mayor de edad");
+                }
+            }
+        }
+    }
+}
